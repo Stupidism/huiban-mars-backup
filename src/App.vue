d238b25b83244cfdf5390ee4695849fcefd8d013
Stupid <sunfeng32@qq.com>
2018-03-21 20:24:06 +0800

    feat(App): init auth framework

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <script>
+import store from './store';
+
 export default {
+  store,
+  computed: {
+    userInfo() {
+      return this.$store.state.userInfo;
+    },
+  },
+  methods: {
+    checkSession() {
+      console.info('checkSession start');
+      wx.checkSession({
+        success: () => {
+          console.info('checkSession success');
+          this.getUserInfo();
+        },
+        fail: () => {
+          console.info('checkSession fail');
+          this.login();
+        },
+      });
+    },
+
+    login() {
+      console.info('login start');
+      wx.login({
+        success: ({ code }) => {
+          console.info('login success', code);
+          this.getUserInfo();
+        },
+        fail() {
+          console.info('login fail');
+        },
+      });
+    },
+
+    getUserInfo() {
+      console.info('getUserInfo start');
+      wx.getUserInfo({
+        success: (res) => {
+          console.info('getUserInfo success');
+          this.$store.commit('setUserInfo', res.userInfo);
+        },
+        fail(error) {
+          console.info('getUserInfo fail', error);
+          wx.openSetting();
+        },
+      });
+    },
+  },
   created() {
     // 调用API从本地缓存中获取数据
     const logs = wx.getStorageSync('logs') || [];
     logs.unshift(Date.now());
     wx.setStorageSync('logs', logs);
 
-    console.log('app created and cache logs by setStorageSync');
+    console.info('app created');
+    this.checkSession();
   },
 };
 </script>
