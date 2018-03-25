f853124f6914c21a947e5363212c857ac669642b
stupid <sunfeng32@qq.com>
2018-03-25 17:47:19 +0800

    feat(new-order): new page

---
+++ src/pages/new-order/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <scroll-view class="page new-order">
+    <meeting-card :meeting="meeting" ></meeting-card>
+  </scroll-view>
+</template>
+
+<script>
+import getMeeting from '@/mixins/get-meeting';
+import MeetingCard from '@/components/MeetingCard';
+
+export default {
+  data() {
+    return {
+      meeting: {},
+    };
+  },
+  computed: {
+    amount() {
+      return this.$root.$mp.query.amount;
+    },
+    ticketGrade() {
+      return this.meeting.ticketGrades;
+    },
+  },
+  mixins: [getMeeting],
+  components: { MeetingCard },
+  mounted() {
+    wx.setNavigationBarTitle({
+      title: '订单确认及支付',
+    });
+    this.getMeeting(this.$root.$mp.query.meeting || 1);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
