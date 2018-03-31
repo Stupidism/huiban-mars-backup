285432d9f8fc6095725ae6f072d5e4c090ff5c8c
Stupid <sunfeng32@qq.com>
2018-04-01 03:05:39 +0800

    feat(api/payment-methods): new api

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,7 +2,10 @@ import wxRequest from './wxRequest';
 
 import meetingGet from './meetings/get';
 
+import paymentMethodList from './payment-methods/list';
+
 export {
   wxRequest,
   meetingGet,
+  paymentMethodList,
 };
---
+++ src/apis/payment-methods/list.js
@@ -0,0 +1,10 @@
+export const example = [{
+  name: '微信',
+  desc: '推荐已安装微信的用户使用',
+  iconImg: '微信',
+  thirdPartyPayeeId: 'xxxxxxx',
+}];
+
+export default () => ({
+  url: '/payment-methods',
+});
