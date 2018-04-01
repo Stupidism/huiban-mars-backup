5b7447e0ddd759d77edec3790ec9199b73baf5aa
Stupid <sunfeng32@qq.com>
2018-04-02 01:05:51 +0800

    feat(apis): post transactions and orders

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,11 +1,14 @@
 import wxRequest from './wxRequest';
 
 import meetingGet from './meetings/get';
-
+import orderPost from './orders/post';
 import paymentMethodList from './payment-methods/list';
+import transactionPost from './transactions/post';
 
 export {
   wxRequest,
   meetingGet,
+  orderPost,
   paymentMethodList,
+  transactionPost,
 };
