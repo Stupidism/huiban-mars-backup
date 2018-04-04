295fe701024c3fae083124fd7687315f52108970
Stupid <sunfeng32@qq.com>
2018-04-04 11:43:38 +0800

    feat(order-detail): new page

---
+++ src/pages/orders/one/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <scroll-view class="page">
+  </scroll-view>
+</template>
+
+<script>
+import getOrder from '@/mixins/get-order';
+
+export default {
+  mixins: [getOrder],
+  async mounted() {
+    wx.setNavigationBarTitle({
+      title: '订单详情',
+    });
+    await this.getOrder(this.$root.$mp.query.id || 1);
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
