7cb21b67191d5dd1e440c4a449d1700df30085bc
Stupid <sunfeng32@qq.com>
2018-04-09 01:50:55 +0800

    feat(modules): visualize cash

---
+++ src/modules/Cash.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>{{formattedAmount}}</span>
+</template>
+<script>
+import toCash from '@/utils/filters/cash';
+
+export default {
+  props: ['amount'],
+  computed: {
+    formattedAmount() {
+      return toCash(this.amount);
+    }
+  },
+};
+</script>
