2c4b140d0fe4c8045f067f67a3a83fac8ff2c033
Stupid <sunfeng32@qq.com>
2018-04-09 01:50:39 +0800

    feat(modules): DateTime and Date

---
+++ src/modules/Date.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>{{formattedTime}}</span>
+</template>
+<script>
+import DateTime from './DateTime';
+
+export default {
+  props: {
+    format: {
+      default: 'YYYY-MM-DD',
+    },
+  },
+  extends: DateTime,
+};
+</script>
+<style>
+</style>
---
+++ src/modules/DateTime.vue
@@ -0,0 +1,25 @@
+<template>
+  <span>{{formattedTime}}</span>
+</template>
+<script>
+import format from 'date-fns/format';
+
+export default {
+  props: {
+    time: {
+      required: true,
+    },
+    format: {
+      default: 'YYYY-MM-DD HH:mm:ss',
+    },
+  },
+  computed: {
+    formattedTime() {
+      return format(this.time, this.format);
+    }
+  },
+};
+
+</script>
+<style>
+</style>
