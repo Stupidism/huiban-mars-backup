1ab4588142d8dd202a33f4ab73e76f09fe066ea5
Stupid <sunfeng32@qq.com>
2018-04-09 23:11:48 +0800

    refactor(theme): use less

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 
 import App from './App';
 import './styles/weui.css';
-import './styles/theme.css';
+import './styles/theme.less';
 
 Vue.config.productionTip = false;
 App.mpType = 'app';
