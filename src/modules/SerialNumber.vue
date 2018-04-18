11f4599671dc5991539e8c1023898b0b890dac96
Stupid <sunfeng32@qq.com>
2018-04-18 22:13:44 +0800

    feat(SerialNumber): simple module

---
+++ src/modules/SerialNumber.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="serial-number">{{number}}</div>
+</template>
+<script>
+export default {
+  props: ['number'],
+};
+</script>
+<style lang="less">
+.serial-number {
+  @size: 17px;
+  width: @size;
+  height: @size;
+  line-height: @size - 1px;
+  border-width: 0;
+  border-radius: @size / 2;
+  display: inline-block;
+  text-align: center;
+  background: #92c8f7;
+  color: white;
+  font-size: 12px;
+}
+</style>
