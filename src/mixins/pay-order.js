7bcf8c40fe67c56df538b7c63166207244441b99
Stupid <sunfeng32@qq.com>
2018-04-20 01:04:01 +0800

    refactor(mixins/pay-order): only inject one method

--- src/mixins/pay-order.js
+++ src/mixins/pay-order.js
@@ -1,47 +1,54 @@
 import qs from 'query-string';
 import { wxRequest, transactionPost } from '@/apis';
 
+const onPaymentSucess = (orderId) => {
+  wx.navigateTo({
+    url: `/pages/new-order/payment-result/main?${qs.stringify({
+      order: orderId || 1,
+    })}`,
+  });
+};
+
+const onPaymentFail = error => console.error(error);
+
+const postTransaction = async (orderId) => {
+  try {
+    return await wxRequest(transactionPost(orderId));
+  } catch (e) {
+    if (e.errMsg === 'request:fail url not in domain list') {
+      return {};
+    }
+    console.error(e.statusCode, e.data);
+    throw e;
+  }
+};
+
+const payTransaction = (transaction) => {
+  console.info('transaction', transaction);
+  if (!transaction.thirdPartyTransactionInfo) {
+    onPaymentSucess(1);
+    return;
+  }
+
+  wx.requestPayment({
+    ...transaction.thirdPartyTransactionInfo,
+    success: (res) => {
+      onPaymentSucess(res);
+    },
+    fail: (res) => {
+      onPaymentFail(res);
+    }
+  });
+};
+
 export default {
   methods: {
     async payOrder(order) {
       if (!order.transaction || (new Date(order.transaction.expiresAt) < new Date())) {
-        const transaction = await this.postTransaction(order.id);
-        return this.payTransaction(transaction);
+        const transaction = await postTransaction(order.id);
+        return payTransaction(transaction);
       }
-      return this.payTransaction(order.transaction);
-    },
-    async postTransaction(orderId) {
-      try {
-        return await wxRequest(transactionPost(orderId));
-      } catch (e) {
-        if (e.errMsg === 'request:fail url not in domain list') {
-          return {};
-        }
-        console.error(e.statusCode, e.data);
-        throw e;
-      }
-    },
-    payTransaction(transaction) {
-      console.info('transaction', transaction);
-      wx.requestPayment({
-        ...transaction.thirdPartyTransactionInfo,
-        success: (res) => {
-          this.onPaymentSucess(res);
-        },
-        fail: (res) => {
-          this.onPaymentFail(res);
-        }
-      });
-    },
-    onPaymentSucess(order) {
-      wx.navigateTo({
-        url: `./payment-result/main?${qs.stringify({
-          order: order.id || 1,
-        })}`,
-      });
-    },
-    onPaymentFail(error) {
-      console.error(error);
+      return payTransaction(order.transaction);
     },
   }
 };
