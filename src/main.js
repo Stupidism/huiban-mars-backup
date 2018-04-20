add10d278b75ef6fb9a534ee3ed650e8a1cab46b
Stupid <sunfeng32@qq.com>
2018-04-21 01:19:21 +0800

    feat(pages/share-result): init page

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ export default {
       'pages/new-order/payment-result/main',
       'pages/orders/one/main',
       'pages/tickets/main',
+      'pages/tickets/share-result/main',
     ], // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
     window: {
       backgroundTextStyle: 'light',
