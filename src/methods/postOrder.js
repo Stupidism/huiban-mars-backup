4d159909f32455b39d2c7b3219e99d05614ce369
Stupid <5801015+Stupidism@users.noreply.github.com>
2018-05-18 23:49:53 +0800

    fix(postOrder): add default id for newOrder

--- src/methods/postOrder.js
+++ src/methods/postOrder.js
@@ -3,6 +3,7 @@ import { wxRequest, orderPost } from '@/apis';
 export default async function postOrder(orderBody) {
   try {
     const newOrder = await wxRequest(orderPost(orderBody));
+    newOrder.id = newOrder.id || 1;
     return newOrder;
   } catch (e) {
     if (e.errMsg === 'request:fail url not in domain list') {
