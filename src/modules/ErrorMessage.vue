6d630c04f7d277d345a0a335f2d2d3004f7154d7
Stupid <5801015+Stupidism@users.noreply.github.com>
2018-05-29 22:35:35 +0800

    feat(modules/ErrorMessage): use as component

---
+++ src/modules/ErrorMessage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-if="show" class="text small danger">{{message}}</div>
+</template>
+
+<script>
+  export default {
+    props: {
+      show: Boolean,
+      message: String,
+    },
+  };
+</script>
