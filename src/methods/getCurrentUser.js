1e3b7f96fb174783e03f8cdbf023d6d335076d23
Stupid <5801015+Stupidism@users.noreply.github.com>
2018-06-05 00:11:23 +0800

    fix(getCurrentUser): allow login fail and log better

--- src/methods/getCurrentUser.js
+++ src/methods/getCurrentUser.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import { wxRequest, userCurrentGet } from '@/apis';
+import { example as userExample } from '@/apis/users/current';
 
 import login, { loginToWechat } from './login';
 
@@ -17,36 +18,51 @@ const checkSession = () => new Promise((resolve, reject) => {
   });
 });
 
+const promptInternetError = () => wx.showModal({
+  title: '网络异常',
+  content: '无法登录',
+  showCancel: false,
+  success(res) {
+    if (res.confirm) {
+      console.info('user clicked confirm');
+    }
+  },
+});
+
 const getCurrentUser = async () => {
   try {
     await checkSession();
     const user = await wxRequest(userCurrentGet());
     if (_.isEmpty(user)) throw new Error('Empty User');
-    return user;
+    return {
+      ...user,
+      id: 1,
+    };
   } catch (e) {
+    console.error('getCurrentUser fail at first attempt', e);
+    if (e.errMsg !== 'request:fail url not in domain list' && e.statusCode !== 401) {
+      promptInternetError();
+      return {};
+    }
+  }
+
+  console.error('getCurrentUser try to refresh token and fetch again');
+
+  try {
     const wechatCode = await loginToWechat();
     await login({
       type: 'wechat',
       wechatCode,
     });
-    console.info('TODO: use real accessToken instead');
-  }
-
-  try {
     const user = await wxRequest(userCurrentGet());
     return user;
   } catch (e) {
-    wx.showModal({
-      title: '网络异常',
-      content: '无法登录',
-      showCancel: false,
-      success(res) {
-        if (res.confirm) {
-          console.info('user clicked confirm');
-        }
-      },
-    });
-    return {};
+    console.error('getCurrentUser fail at second attempt', e);
+    if (e.errMsg !== 'request:fail url not in domain list') {
+      promptInternetError();
+      return {};
+    }
+    return userExample;
   }
 };
 
