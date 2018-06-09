76232c123e098d1cd57ca2c912b40bc0658eaf99
Feng Sun <stupidism@Fengs-MacBook-Pro.local>
2018-06-09 11:39:42 +0800

    fix(getAuthHeader): omit if no token

--- src/methods/auth.js
+++ src/methods/auth.js
@@ -1,7 +1,11 @@
 export const getAccessToken = () => wx.getStorageSync('access_token');
 
-export const getAuthHeader = () => ({
-  Authorization: `Bearer ${getAccessToken() || ''}`,
-});
+export const getAuthHeader = () => {
+  const token = getAccessToken();
+  if (!token) return {};
+  return {
+    Authorization: `Bearer ${token}`,
+  };
+};
 
 export const setAccessToken = accessToken => wx.setStorageSync('access_token', accessToken);
