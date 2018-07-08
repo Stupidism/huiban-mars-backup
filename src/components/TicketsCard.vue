0dfbe44ff31e664ef2706be17a75b1bf987b5dc7
stupidism <sunfeng32@qq.com>
2018-07-08 17:20:04 +0800

    fix(TicketsCard): giftNum exclude self participated tickets

--- src/components/TicketsCard.vue
+++ src/components/TicketsCard.vue
@@ -2,20 +2,29 @@
   <div class="ticket-card section-no-padding">
     <meeting-card v-if="meeting" :meeting="meeting" />
     <div class="section-single-line">
-      共 {{tickets.length}} 个赠送名额，还剩 <span class="sharable-num">{{sharableTickets.length}}</span> 个
+      共 {{giftNum}} 个赠送名额，还剩 <span class="sharable-num">{{sharableTickets.length}}</span> 个
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
+import { mapState } from 'vuex';
 import MeetingCard from '@/components/MeetingCard';
 
 export default {
   props: ['meeting', 'tickets'],
   computed: {
+    giftNum() {
+      return this.tickets.length - this.selfParticipatingTickets.length;
+    },
+    selfParticipatingTickets() {
+      return _.filter(this.tickets, ['participantId', this.currentUser.id]);
+    },
     sharableTickets() {
       return this.tickets.filter(({ status }) => status === 'no_participant');
     },
+    ...mapState(['currentUser']),
   },
   components: { MeetingCard },
 };
