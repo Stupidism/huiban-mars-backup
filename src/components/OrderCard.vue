d266d27aa0561f6a89c5ce9252f2314f6b375972
stupidism <sunfeng32@qq.com>
2018-07-23 21:15:19 +0800

    feat(order-card): use new order status images

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -2,10 +2,7 @@
   <div class="order-card section-no-padding">
     <div class="status-wrapper">
       <meeting-card v-if="order.meeting" :meeting="order.meeting" />
-      <div class="order-status" :class="order.status">
-        <image :src="statusImage" />
-        <span>{{order.statusDesc}}</span>
-      </div>
+      <image class="order-status" :src="statusImage" />
     </div>
     <div class="section-single-line">
       <div class="order-info flex justified aligned">
@@ -38,7 +35,7 @@ export default {
   props: ['order', 'onCancel'],
   computed: {
     statusImage() {
-      return `/static/order-status/${this.order.status}.png`;
+      return `/static/order-status/${this.order.status}.svg`;
     },
   },
   methods: {
@@ -69,36 +66,9 @@ export default {
     bottom: 15px;
     right: 15px;
 
-    @size: 53px;
+    @size: 80px;
     width: @size;
     height: @size;
-
-    image {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-    }
-
-    span {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%) rotate(-45deg);
-      white-space: nowrap;
-    }
-
-    &.completed {
-      color: #33CC33;
-    }
-    &.to_be_paid {
-      color: #CC0000;
-    }
-    &.cancelled,
-    &.closed {
-      color: #999999;
-    }
   }
 
   .order-info {
