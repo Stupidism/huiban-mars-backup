2e12c6b4ee03d0756611b0d423d28486f184082f
stupidism <sunfeng32@qq.com>
2018-08-31 08:48:47 +0800

    fix(getMeeting): use provided typeColor first

--- src/methods/getMeeting.js
+++ src/methods/getMeeting.js
@@ -5,7 +5,8 @@ const colors = ['blue', 'green', 'orange', 'purple', 'black'];
 export const addTypeColorForTicketGrades = (ticketGrades = []) => ticketGrades.map(
   (ticketGrade, index) => ({
     ...ticketGrade,
-    typeColor: colors[Math.round(index * ((colors.length - 1) / (ticketGrades.length - 1)))],
+    typeColor: ticketGrade.typeColor
+      || colors[Math.round(index * ((colors.length - 1) / (ticketGrades.length - 1)))],
   })
 );
 
